b697731d712083334bc98a14d59bde84408d74f3
rohityadavsolutions <rohityadavsolutions@email.com>
2026-03-17 12:37:31 +0530

    update version to 44

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.arbutusinfotech.upkeep"
         minSdk = 24
         targetSdk = 36
-        versionCode = 43
-        versionName = "4.3"
+        versionCode = 44
+        versionName = "4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
