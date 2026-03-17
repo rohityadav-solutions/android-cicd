b8218f85ddd997d28e253fe25c3ea70844373c03
rohityadavsolutions <rohityadavsolutions@email.com>
2026-03-17 13:14:12 +0530

    update verson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.arbutusinfotech.upkeep"
         minSdk = 24
         targetSdk = 36
-        versionCode = 44
-        versionName = "4.4"
+        versionCode = 45
+        versionName = "4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
