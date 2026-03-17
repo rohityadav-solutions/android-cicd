23e8387e7f875119dccbee0a07f131766a29766b
rohityadavsolutions <rohityadavsolutions@email.com>
2026-03-17 13:31:12 +0530

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.arbutusinfotech.upkeep"
         minSdk = 24
         targetSdk = 36
-        versionCode = 45
-        versionName = "4.5"
+        versionCode = 46
+        versionName = "4.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
